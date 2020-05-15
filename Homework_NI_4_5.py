764d6d3ef217e8b66154ba7e33fcb246c96b1f56
Natalia <NI_edu@mail.ru>
2020-05-16 02:15:34 +0300

    Урок 4

---
+++ Homework_NI_4_5.py
@@ -0,0 +1,11 @@
+# Task 5
+from functools import reduce
+
+new_list = [i for i in range(100, 1001) if i % 2 == 0]
+
+
+def my_func(prev_el, el):
+    return prev_el * el
+
+
+print(reduce(my_func, new_list))
